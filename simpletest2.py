d35f4a2318c2f858218907f22ce1cf60af96bc23
Shaun-Kavanagh <shaunkavanaghwork@gmail.com>
2018-12-17 00:49:06 +0000

    latest working version

--- simpletest2.py
+++ simpletest2.py
@@ -57,14 +57,14 @@ is_recording = [False,False,False,False,False,False,False,False,False,False,Fals
 is_playing = [False,False,False,False,False,False,False,False,False,False,False,False]
 my_thread_record = None
 my_thread_play = None
+is_pygame_init = False
+my_thread_init = None
+audio = pyaudio.PyAudio() # create pyaudio instantiation
 
-
-
-
-def loop_record(x):
+def loop_record2(x):
     form_1 = pyaudio.paInt16 # 16-bit resolution
     chans = 1 # 1 channel
-    samp_rate = 48000 # 44.1kHz sampling rate
+    samp_rate = 44100 # 44.1kHz sampling rate
     chunk = 4096 # 2^12 samples for buffer
     record_secs = 120 # seconds to record
     dev_index = 2 # device index found by p.get_device_info_by_index(ii)
@@ -74,11 +74,15 @@ def loop_record(x):
     audio = pyaudio.PyAudio() # create pyaudio instantiation
     print("recording")
     frames = []
+    
+        
 
     #create pyaudio stream
     stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
                     input_device_index = dev_index,input = True, \
                     frames_per_buffer=chunk)
+    
+        
     i = 0
     while is_recording[x]:
         print(i)
@@ -97,6 +101,51 @@ def loop_record(x):
     wavefile.writeframes(b''.join(frames))
     wavefile.close()
 
+def loop_record(x):
+    #global is_recording
+    #global my_thread_record
+    form_1 = pyaudio.paInt16 # 16-bit resolution
+    chans = 1 # 1 channel
+    #samp_rate = 44100 # 44.1kHz sampling rate
+    samp_rate = 48000
+    chunk = 1024 # 2^12 samples for buffer
+    dev_index = 2 # device index found by p.get_device_info_by_index(ii)
+    file_array = ['test0.wav','test1.wav','test2.wav','test3.wav','test4.wav','test5.wav','test6.wav','test7.wav','test8.wav','test9.wav','test10.wav']
+    wav_output_filename = file_array[x] # name of .wav file
+    #audio = pyaudio.PyAudio() # create pyaudio instantiation
+    print("recording")
+    frames = []
+
+    if(is_recording[x]):
+        print("Yeah")
+
+    #create pyaudio stream
+    stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
+                    input_device_index = dev_index,input = True, \
+                    frames_per_buffer=chunk)
+    
+    if(is_recording[x]):
+        print("Yeah after")
+    i = 0
+    while is_recording[x]:
+        print(i)
+        data = stream.read(chunk, exception_on_overflow = False)
+        #data = stream.read(chunk)
+
+        frames.append(data)
+        i = i+1
+    
+    print("finished recording")
+    stream.stop_stream()
+    stream.close()
+    #audio.terminate()
+    wavefile = wave.open(wav_output_filename,'wb')
+    wavefile.setnchannels(chans)
+    wavefile.setsampwidth(audio.get_sample_size(form_1))
+    wavefile.setframerate(samp_rate)
+    wavefile.writeframes(b''.join(frames))
+    wavefile.close()
+    
 def press_button_record(x):
     global is_recording
     global my_thread_record
@@ -112,6 +161,8 @@ def press_button_stop(x):
 
     if is_recording[x]:
         is_recording[x] = False
+        print ("this is x: " )
+        print (x)
         press_button_play(x)
         
         my_thread_record.join()
@@ -130,53 +181,120 @@ def press_button_stop_play(x):
     global my_thread_play
 
     if is_playing[x]:
+        print ("in is playing: ")
+        print(x)
         is_playing[x] = False
         my_thread_play.join()
 
-    if (x==11):
+    
+    if (x==11) :
         is_playing = [False,False,False,False,False,False,False,False,False,False,False,False]
     
-    
-       
+
 
 def loop_play(x):
     while is_playing[x]:
         play_audio(x)
-        
+
+def pygameInit(x):
+    global is_pygame_init
+    global my_thread_init
+
+    if (x==0):
+        is_pygame_init = True
+        pygame.init()
+        pygame.mixer.pre_init(44100, -16, 1, 512)
+        pygame.mixer.set_num_channels(12)
+    else:
+        is_pygame_init = False
+        my_thread_init.join()
 def play_audio(x):
+    global is_playing
+    global my_thread_play
+    global is_recording
+
+    #if not is_pygame_init:
+     #   i =0
+      #  my_thread_init = threading.Thread(target=pygameInit, args=(i,))
+       # my_thread_init.start()
     pygame.init()
-    pygame.mixer.pre_init(44100, -16, 2, 512)
-    
+    pygame.mixer.pre_init(44100, -16, 1, 512)
+    pygame.mixer.set_num_channels(12)
     SOUND_MAPPING = {
-      0: '/home/pi/Adafruit_Python_MPR121/examples/test0.wav',
-      1: '/home/pi/Adafruit_Python_MPR121/examples/test1.wav',
-      2: '/home/pi/Adafruit_Python_MPR121/examples/test2.wav',
-      3: '/home/pi/Adafruit_Python_MPR121/examples/test3.wav',
-      4: '/home/pi/Adafruit_Python_MPR121/examples/test4.wav',
-      5: '/home/pi/Adafruit_Python_MPR121/examples/test5.wav',
-      6: '/home/pi/Adafruit_Python_MPR121/examples/test6.wav',
-      7: '/home/pi/Adafruit_Python_MPR121/examples/test7.wav',
-      8: '/home/pi/Adafruit_Python_MPR121/examples/test8.wav',
-      9: '/home/pi/Adafruit_Python_MPR121/examples/test9.wav',
-      10: '/home/pi/Adafruit_Python_MPR121/examples/test10.wav',
-      11: '/home/pi/Adafruit_Python_MPR121/examples/test11.wav',
+      0: '/home/pi/Adafruit_Python_MPR121/examples/Fruity-Loops/test0.wav',
+      1: '/home/pi/Adafruit_Python_MPR121/examples/Fruity-Loops/test1.wav',
+      2: '/home/pi/Adafruit_Python_MPR121/examples/Fruity-Loops/test2.wav',
+      3: '/home/pi/Adafruit_Python_MPR121/examples/Fruity-Loops/test3.wav',
+      4: '/home/pi/Adafruit_Python_MPR121/examples/Fruity-Loops/test4.wav',
+      5: '/home/pi/Adafruit_Python_MPR121/examples/Fruity-Loops/test5.wav',
+      6: '/home/pi/Adafruit_Python_MPR121/examples/Fruity-Loops/test6.wav',
+      7: '/home/pi/Adafruit_Python_MPR121/examples/Fruity-Loops/test7.wav',
+      8: '/home/pi/Adafruit_Python_MPR121/examples/Fruity-Loops/test8.wav',
+      9: '/home/pi/Adafruit_Python_MPR121/examples/Fruity-Loops/test9.wav',
+      10: '/home/pi/Adafruit_Python_MPR121/examples/Fruity-Loops/test10.wav',
+      11: '/home/pi/Adafruit_Python_MPR121/examples/Fruity-Loops/test11.wav',
     }
     sounds = [0,0,0,0,0,0,0,0,0,0,0,0]
 
     for key,soundfile in SOUND_MAPPING.iteritems():
         sounds[key] =  pygame.mixer.Sound(soundfile)
         sounds[key].set_volume(1);
+    
+    file_array = ['test0.wav','test1.wav','test2.wav','test3.wav','test4.wav','test5.wav','test6.wav','test7.wav','test8.wav','test9.wav','test10.wav']
+    #print ("playing" + file_array[x])
+    while is_playing[x] and not pygame.mixer.Channel(x).get_busy() and not is_recording[x]:
+        #while not pygame.mixer.Channel(x).get_busy():
+   # while is_playing[x]:  not pygame.mixer.Channel(x).get_busy()
+        
+            pygame.mixer.Channel(x).play(sounds[x])
+            print ("playing" + file_array[x])
+    #wav_output_filename = file_array[x] # name of .wav file
+    #wf = wave.open(wav_output_filename, 'rb')
+    #p = pyaudio.PyAudio()
+    #chunk = 4096
+    #stream = p.open(
+     #   format = p.get_format_from_width(wf.getsampwidth()),
+     #   channels = wf.getnchannels(),
+     #   rate = wf.getframerate(),
+     #   output = True)
+
+   # data = wf.readframes(chunk)
+
+   # while data != '' and is_playing[x]: # is_playing to stop playing
+   #     stream.write(data)
+   #     data = wf.readframes(chunk)
+
+    #stream.stop_stream()
+    #stream.close()
+    #p.terminate()
+    
+    
+def play_audio2(x):
     global is_playing
     global my_thread_play
+    chunk = 4096
     file_array = ['test0.wav','test1.wav','test2.wav','test3.wav','test4.wav','test5.wav','test6.wav','test7.wav','test8.wav','test9.wav','test10.wav']
     print ("playing" + file_array[x])
-    while is_playing[x]:
-        sounds[x].play()
-        #print ("playing" + file_array[x])
-    
-    
+    wav_output_filename = file_array[x] # name of .wav file
+    wf = wave.open(wav_output_filename, 'rb')
+    p = pyaudio.PyAudio()
 
+    stream = p.open(
+        format = p.get_format_from_width(wf.getsampwidth()),
+        channels = wf.getnchannels(),
+        #rate = wf.getframerate(),
+        rate = 44100,
+        output = True)
 
+    data = wf.readframes(chunk)
+
+    while data != '' and is_playing[x]: # is_playing to stop playing
+        stream.write(data)
+        data = wf.readframes(chunk)
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
 
 while True:
     current_touched = cap.touched()
@@ -188,60 +306,18 @@ while True:
         # First check if transitioned from not touched to touched.
         if current_touched & pin_bit and not last_touched & pin_bit:
             print('{0} touched!'.format(i))
-            if(i==0):
+            if(i==11):
+                press_button_stop_play(i)
+            elif (i==10):
+                press_button_stop_play(i)
+            else:
                 press_button_stop_play(i)
                 press_button_record(i)
-                press_button_stop_play(i)
-            elif(i==1):
-                press_button_stop_play(i)
-                press_button_record(i)
-                press_button_stop_play(i)
-            elif(i==2):
-                press_button_stop_play(i)
-                press_button_record(i)
-                press_button_stop_play(i)
-            elif(i==3):
-                press_button_stop_play(i)
-                press_button_record(i)
-                press_button_stop_play(i)
-            elif(i==4):
-                press_button_stop_play(i)
-                press_button_record(i)
-                press_button_stop_play(i)
-            elif(i==5):
-                press_button_stop_play(i)
-                press_button_record(i)
-                press_button_stop_play(i)
-            elif(i==6):
-                press_button_stop_play(i)
-                press_button_record(i)
-                press_button_stop_play(i)
-            elif(i==7):
-                press_button_stop_play(i)
-                press_button_record(i)
-                press_button_stop_play(i)
-            elif(i==11):
-                press_button_stop_play(i)
-                
+
         # Next check if transitioned from touched to not touched.
         if not current_touched & pin_bit and last_touched & pin_bit:
             print('{0} released!'.format(i))
-            if(i==0):
-                press_button_stop(i)
-            elif(i==1):
-                press_button_stop(i)
-            elif(i==2):
-                press_button_stop(i)
-            elif(i==3):
-                press_button_stop(i)
-            elif(i==4):
-                press_button_stop(i)
-            elif(i==5):
-                press_button_stop(i)
-            elif(i==6):
-                press_button_stop(i)
-            elif(i==7):
-                press_button_stop(i)
+            press_button_stop(i)
     # Update last state and wait a short period before repeating.
     last_touched = current_touched
     time.sleep(0.1)
